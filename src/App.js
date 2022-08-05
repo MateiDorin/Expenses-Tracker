6559d02f91522c40af5a20d8d6565511f81d9e15
Dorin Matei <matei.dorin22@gmail.com>
2022-08-05 09:16:35 +0100

    fetch data from backend

--- src/App.js
+++ src/App.js
@@ -6,37 +6,31 @@ import SignIn from "./components/SignIn";
 import Filter from "./components/Filter";
 import Wrapper from "./components/Helpers/Wrapper";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "a1",
-    title: "Car Insurance",
-    amount: 232.15,
-    date: new Date(2021, 10, 22),
-  },
-  {
-    id: "a2",
-    title: "Iphone",
-    amount: 1250,
-    date: new Date(2021, 11, 25),
-  },
-  {
-    id: "a3",
-    title: "Book",
-    amount: 15.99,
-    date: new Date(2021, 0, 15),
-  },
-  {
-    id: "a4",
-    title: "Laptop Acer",
-    amount: 850,
-    date: new Date(2022, 5, 15),
-  },
-];
+
+const DUMMY_EXPENSES = [];
+
+async function fetchExpenses() {
+  const response = await fetch(
+    "https://expenses-tracker-8297c-default-rtdb.europe-west1.firebasedatabase.app/expenses.json"
+  );
+  const expenses1 = await response.json();
+  Object.keys(expenses1).forEach((key) => {
+    return DUMMY_EXPENSES.push({
+      id: expenses1[key].id,
+      title: expenses1[key].title,
+      amount: expenses1[key].amount,
+      date: new Date(expenses1[key].date.toLocaleString()),
+    });
+  });
+}
+
+fetchExpenses();
+
 
 const App = () => {
   const [validUser, setValidUser] = useState(false);
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-  const [year, setYear] = useState('2022');
+  const [year, setYear] = useState("2022");
 
   const newExpense = (newData) => {
     setExpenses((prevExpenses) => {
@@ -53,27 +47,27 @@ const App = () => {
   };
 
   const selectedYear = (event) => {
-    setYear(event)
-  }
-
-
+    setYear(event);
+  };
 
   return (
     <Wrapper>
-        {!validUser && (
-          <div>
-            <SignIn start={startProgram}/>
-          </div>
-        )}
+      {!validUser && (
+        <div>
+          <SignIn start={startProgram} />
+        </div>
+      )}
       {validUser && (
         <div>
           <div className="sign-out">
-            <button onClick={stopProgram} className="out">Sign Out</button>
+            <button onClick={stopProgram} className="out">
+              Sign Out
+            </button>
           </div>
           <p className="app-name">Expenses Tracker</p>
           <InsertExpense onSaveExpenseData={newExpense} />
-          <Filter onSelectedYear={selectedYear}/>
-          <ExpenseItem items={expenses} newYear = {year} />
+          <Filter onSelectedYear={selectedYear} />
+          <ExpenseItem items={expenses} newYear={year} />
         </div>
       )}
     </Wrapper>
